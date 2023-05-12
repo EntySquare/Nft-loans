53b348bda95e825b1a64845ec605ccfec54edef3
zhcyueliyangzi <yueliyangzi@163.com>
2023-05-12 15:42:45 +0800

    nft

---
+++ routing/app/nft.go
@@ -0,0 +1,40 @@
+package app
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"nft-loans/config"
+	"nft-loans/pkg"
+	"nft-loans/routing/types"
+)
+
+func Deposit(c *fiber.Ctx) error {
+	reqParams := types.LoginAndRegisterReq{}
+	err := c.BodyParser(&reqParams)
+	if err != nil {
+		return c.JSON(pkg.MessageResponse(config.MESSAGE_FAIL, "parser error", ""))
+	}
+
+	returnT := pkg.RandomString(64)
+
+	return c.JSON(pkg.SuccessResponse(returnT))
+}
+func Withdraw(c *fiber.Ctx) error {
+	reqParams := types.LoginAndRegisterReq{}
+	err := c.BodyParser(&reqParams)
+	if err != nil {
+		return c.JSON(pkg.MessageResponse(config.MESSAGE_FAIL, "parser error", ""))
+	}
+	returnT := pkg.RandomString(64)
+
+	return c.JSON(pkg.SuccessResponse(returnT))
+}
+func Approve(c *fiber.Ctx) error {
+	reqParams := types.LoginAndRegisterReq{}
+	err := c.BodyParser(&reqParams)
+	if err != nil {
+		return c.JSON(pkg.MessageResponse(config.MESSAGE_FAIL, "parser error", ""))
+	}
+	returnT := pkg.RandomString(64)
+
+	return c.JSON(pkg.SuccessResponse(returnT))
+}
--- routing/router.go
+++ routing/router.go
@@ -3,16 +3,22 @@ package routing
 import (
 	"github.com/gofiber/fiber/v2"
 	"nft-loans/routing/app"
+	intcpt "nft-loans/routing/intercept"
 )
 
 func Setup(f *fiber.App) {
 	appApi := f.Group("/app")
 
-	AppProductSetUp(appApi)
+	AppUserSetUp(appApi)
+	AppNftSetUp(appApi)
 
 }
 
-// AppSetUp
-func AppProductSetUp(appApi fiber.Router) {
-	appApi.Post("/login", app.LoginAndRegister) //获取btc涨幅和usdt价
+func AppUserSetUp(appApi fiber.Router) {
+	appApi.Post("/login", app.LoginAndRegister) //login
+}
+func AppNftSetUp(appApi fiber.Router) {
+	appApi.Post("/nft/deposit", intcpt.AuthApp(), app.Deposit)
+	appApi.Post("/nft/withdraw", intcpt.AuthApp(), app.Withdraw)
+	appApi.Post("/nft/approve", intcpt.AuthApp(), app.Approve)
 }
